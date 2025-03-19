da07d4780747a2af5308baa754bfc046910305b5
unknown <mateox463@gmail.com>
2025-03-19 10:41:53 -0300

    se añade libreria para tailwindCss y se crea layout de navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar bg-[#2f314e] shadow-sm">
+      <div className="navbar-start">
+        <a className="" href="/">
+          <figure>
+            <img
+              src="/warp.png"
+              alt="logo"
+              className="w-[50px] h-[50px] md:w-[70px] md:h-[70px] lg:w-[90px] lg:h-[90px]"
+            />
+          </figure>
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+          <li>
+            <a>Eventos</a>
+          </li>
+          <li>
+            <a href="/contacto">Contacto</a>
+          </li>
+          <li>
+            <a href="/about">Sobre Nosotros</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="navbar-end gap-2">
+        <a className="btn">Iniciar Sesion</a>
+        <a className="btn">Registrarse</a>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Card from "../components/Card";
 
 const HomePage = () => {
   return (
     <div>
       <h1 className="text-7xl font-bold underline">Home</h1>
+      <Card />
     </div>
   );
 };
