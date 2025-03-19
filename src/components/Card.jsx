269bc035723ff6615fb2852808434cacdf43893d
unknown <mateox463@gmail.com>
2025-03-19 11:41:47 -0300

    se implementan principios de cards footer y hero

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,21 +2,22 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div className="card bg-base-100 w-96 shadow-sm">
+    <div className="card bg-white shadow-lg hover:shadow-5xl transition-shadow hover:-translate-y-2 transition-transform duration-600">
       <figure>
         <img
           src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
           alt="Shoes"
+          className="w-full h-44 object-cover"
         />
       </figure>
-      <div className="card-body">
+      <div className="card-body text-black ">
+        <p className="text-[#444444]">UBICACION</p>
         <h2 className="card-title">Card Title</h2>
-        <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
-        </p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+        <h3>Fecha</h3>
+        <div className="card-actions ">
+          <button className="btn bg-[#2F314E] shadow-lg shadow-indigo-500/50">
+            Buy Now
+          </button>
         </div>
       </div>
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,8 +4,39 @@ import Card from "../components/Card";
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-7xl font-bold underline">Home</h1>
-      <Card />
+      <div className="hero text-black bg-white">
+        <div className="hero-content flex-col lg:flex-row">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Bienvenido a WarpPass!</h1>
+            <p className="py-6">Tu sitio de venta de entradas Favorito</p>
+            <button className="btn btn-primary">Mira los eventos</button>
+          </div>
+        </div>
+      </div>
+      <div className="py-6 px-30">
+        <h2 className="text-black text-3xl font-bold uppercase text-center">
+          Eventos disponibles
+        </h2>
+
+        <div className="m-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </div>
   );
 };
