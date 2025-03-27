8a23e5e2006b39425709e30df8ce9e76173d830f
unknown <mateox463@gmail.com>
2025-03-27 17:38:02 -0300

    New: Layout de AdminPage

--- src/pages/AdminPages/HomeAdmin.jsx
+++ src/pages/AdminPages/HomeAdmin.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const HomeAdmin = () => {
-  return <div>HomeAdmin</div>;
+  const navigate = useNavigate();
+
+  const handlePanelUsers = () => {
+    navigate("/admin/panelUsers");
+  };
+
+  const handlePanelEvents = () => {
+    navigate("/admin/panelEvents");
+  };
+  return (
+    <div>
+      <div>
+        <h1 className="text-center text-4xl font-bold text-[#b2b205] p-4">
+          Panel de Administracion
+        </h1>
+      </div>
+      <div className="flex justify-center gap-4 p-4 ">
+        <button
+          onClick={handlePanelUsers}
+          className="btn bg-[#2F314E] shadow-lg shadow-indigo-500/50 hover:bg-[#1A1B2D] transition-colors duration-300 active:scale-95 transition-all duration-300"
+        >
+          Panel de usuarios
+        </button>
+        <button
+          onClick={handlePanelEvents}
+          className="btn bg-[#2F314E] shadow-lg shadow-indigo-500/50 hover:bg-[#1A1B2D] transition-colors duration-300 active:scale-95 transition-all duration-300"
+        >
+          Panel de Eventos
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default HomeAdmin;
